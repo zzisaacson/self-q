b088059833cdd8f752fd44f29ee6ffc9d6b1d4bb
zachisaacson <zachary.isaacson@wustl.edu>
2020-08-17 09:46:51 -0500

    firebase

--- App.js
+++ App.js
@@ -3,14 +3,25 @@ import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 
 import Home from './screens/Home' 
 import AddSet from './screens/AddSet';
 
+import firebase from 'firebase';
+import { firebaseConfig } from './components/config';
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+
+
 export default function App() { 
   
   const [isAddMode, setIsAddMode]= useState(false);
   const [courseGoals, setCourseGoals] = useState([]);
 
+  const setGoalsHanlder=goals=>{
+    db.ref().set(goals);
+    return setCourseGoals(goals)
+  }
+
   screen = <Home setScreen = {setIsAddMode} qList ={courseGoals} setQList={setCourseGoals}/>
   if (isAddMode){
-    screen = <AddSet setScreen = {setIsAddMode} qList ={courseGoals} setQList={setCourseGoals}/>
+    screen = <AddSet setScreen = {setIsAddMode} qList ={courseGoals} setQList={setGoalsHanlder}/>
   }
   return (
     screen
---
+++ components/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyC9IrMEpuMkTgDdYnXzay5PnZ1MYgrqeyU",
+    authDomain: "self-q.firebaseapp.com",
+    databaseURL: "https://self-q.firebaseio.com",
+    projectId: "self-q",
+    storageBucket: "self-q.appspot.com",
+    messagingSenderId: "815667248878",
+    appId: "1:815667248878:web:5101ab55ce6deac57ffdff",
+    measurementId: "G-C9QTCTBDSL"
+  };
