b69834e57c9c81865871b3d1f9d367594fd704f8
zzisaacson <zachary.isaacson@yahoo.com>
2021-01-29 14:46:12 -0600

    web build

---
+++ web-build/precache-manifest.45a3e44d359ed25983de0fedaa992d6d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "e34bdc63a3f6187cab2140b5fa45931c",
+    "url": "/self-q/static/media/selfq_custom.89d6291b.png"
+  },
+  {
+    "revision": "8938e2d39f938ef4fbb0",
+    "url": "/self-q/static/js/app.26da0c8e.chunk.js"
+  },
+  {
+    "revision": "709e22aa4da0e05071f78b3c8792455a",
+    "url": "/self-q/static/media/settings.4564248b.png"
+  },
+  {
+    "revision": "3bda0e724af62d70ee336322ac8a388d",
+    "url": "/self-q/static/media/left_arrow.947c4ae8.png"
+  },
+  {
+    "revision": "6b4916847cfa41b4e768da0df0dca0e4",
+    "url": "/self-q/static/media/right_arrow.05efbab9.png"
+  },
+  {
+    "revision": "40e643146fd34e98100d1c34e439b149",
+    "url": "/self-q/static/media/swipe_btn.75af9935.png"
+  },
+  {
+    "revision": "30f4726af345a78af635d3962e77ed00",
+    "url": "/self-q/static/media/self-q.1050f68b.png"
+  },
+  {
+    "revision": "f0015d3f9035458012816d3d3fa195b8",
+    "url": "/self-q/static/media/selfq_science.8475672d.png"
+  },
+  {
+    "revision": "ce4f41b8620d766d6ba67b6832dd4c54",
+    "url": "/self-q/static/media/selfq_math.3a01f113.png"
+  },
+  {
+    "revision": "55e51aeff3b8e5ba81321b3284531e03",
+    "url": "/self-q/static/media/selfq_reading.7d79b493.png"
+  },
+  {
+    "revision": "e20a898e5909151a05b3ab0e5c65a683",
+    "url": "/self-q/static/media/selfq_writing.9ca6bb68.png"
+  },
+  {
+    "revision": "c9b0e2ee670e41428cfa27e409e4edbe",
+    "url": "/self-q/static/media/selfq_social.07bec3e5.png"
+  },
+  {
+    "revision": "c47da2434cd6b943f0b9de3339c25008",
+    "url": "/self-q/static/media/academic_social.8c7f8345.png"
+  },
+  {
+    "revision": "982982312e58defc1cb7e6aaa5aee04b",
+    "url": "/self-q/static/media/selfq_emotional.6fa4cdf5.png"
+  },
+  {
+    "revision": "7750af572876a58116fa",
+    "url": "/self-q/static/js/runtime~app.8d09ddff.js"
+  },
+  {
+    "revision": "0b533f244cb60fb88af97b005e01a28f",
+    "url": "/self-q/static/media/row_btn.cfd107e8.png"
+  },
+  {
+    "revision": "384bd8429cac47f41575045694dfae6a",
+    "url": "/self-q/static/media/home.f2a9ae9d.png"
+  },
+  {
+    "revision": "7a42600dfb861dece0a9f8fe07e1660f",
+    "url": "/self-q/static/media/document.37fa493b.png"
+  },
+  {
+    "revision": "5563e3e72858652a794eea66ceee96ac",
+    "url": "/self-q/static/media/classroom.c7ea54d6.png"
+  },
+  {
+    "revision": "8834fccc3482eeba4a84a402bf7e7789",
+    "url": "/self-q/static/js/2.f64d03f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53ad66a3d9b4078d92b9",
+    "url": "/self-q/static/js/2.f64d03f2.chunk.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/self-q/serve.json"
+  },
+  {
+    "revision": "56124839e178687a83e41e439ee3e820",
+    "url": "/self-q/register-service-worker.js"
+  },
+  {
+    "revision": "09187197b4e3f6dc57cfe035a903135e",
+    "url": "/self-q/manifest.json"
+  },
+  {
+    "revision": "554c3d005fdb98a79c2c9065f8a55a11",
+    "url": "/self-q/index.html"
+  },
+  {
+    "revision": "5e52768b0f7cbb62cbe07751add33101",
+    "url": "/self-q/favicon.ico"
+  },
+  {
+    "revision": "3d85359b37ca7590f8e91d593d02ddaf",
+    "url": "/self-q/favicon-32.png"
+  },
+  {
+    "revision": "afd5733ac789ab1795f1ed7a26e1ecd3",
+    "url": "/self-q/favicon-16.png"
+  },
+  {
+    "revision": "f328c398676e066fb4e76f0c028592b4",
+    "url": "/self-q/expo-service-worker.js"
+  }
+];
